1b3a2f39f4ff9188b0cdd097c464fe34feb03178
toniherold <toniherold90@gmail.com>
2024-04-05 17:17:48 -0300

    Model adjustment 2

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
         scene.add(light);
 
         const fly = await loadGLTF("./assets/models/fly/scene.gltf");
-        fly.scene.scale.set(2, 2, 2);
+        fly.scene.scale.set(0.5, 0.5, 0.5);
         fly.scene.position.set(0, 0, 0);
-        fly.scene.rotation.set(0, 90*Math.PI, 0);
+        fly.scene.rotation.set(90*Math.PI, 0, 0);
 
         const flyAnchor = mindarThree.addAnchor(0);
         flyAnchor.group.add(fly.scene);
